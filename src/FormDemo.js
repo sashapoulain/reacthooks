2b7dbc584f8b20e72a030c2d873922be55062eb1
Sasha Poulain <sashapoulain5@gmail.com>
2024-01-15 15:11:55 +0300

    add ev.preventdefault to keep the data on the same page (because it s a single page application)

--- src/FormDemo.js
+++ src/FormDemo.js
@@ -5,12 +5,17 @@ export default class FormDemo extends Component {
    onChangeHandler = (event) => {
       this.setState({userName:event.target.value})
    }
+   onSubmitHandler = (ev) => {
+      ev.preventDefault();
+      alert(this.state.userName)
+   }
   render() {
     return (
       <div>
-         <form>
+         <form onSubmit={this.onSubmitHandler}>
             <input onChange={this.onChangeHandler} type='text'/>
             <h3>User name is {this.state.userName}</h3>
+            <input type='submit' value='submit'/>
          </form>
       </div>
     )
